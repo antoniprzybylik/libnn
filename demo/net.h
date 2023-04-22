81d8b1b6c4e980e0f99b6236f0f55148adca34a1
Antoni Przybylik <antoni.przybylik@wp.pl>
2023-04-22 14:13:56 +0200

    Added license and author information to files in demo.

--- demo/net.h
+++ demo/net.h
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0
+// Author: Antoni Przybylik
+
 #ifndef NET_H_
 #define NET_H_
 
--- demo/training.h
+++ demo/training.h
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0
+// Author: Antoni Przybylik
+
 #ifndef TRAINING_H_
 #define TRAINING_H_
 
