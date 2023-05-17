790e6a5c9ae18d1f5ee48add9740f3f7f6f6087d
Antoni Przybylik <antoni.przybylik@wp.pl>
2023-05-17 14:08:38 +0200

    Step, cost, and gradient norm can be stored in a file.

---
+++ demo/dumb_steepest_descent/log_plot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+import matplotlib.pyplot as plt
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    with open(args.filename, 'r') as fp:
+        data = json.load(fp)
+        x = [x for x in range(1, len(data)+1)]
+
+        plt.plot(x, data)
+        plt.yscale('symlog')
+        plt.grid()
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
